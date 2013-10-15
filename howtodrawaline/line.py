68549dc1c138d49a75d55102ab0f5c652b929100
Daniel Dunbar <daniel_dunbar@apple.com>
2013-10-14 21:44:35 -0700

    Add a version of the correct OpenGL line algorithm.

--- howtodrawaline/line.py
+++ howtodrawaline/line.py
@@ -130,3 +130,65 @@ def ogl_line2d_2(pt0, pt1, set_last_line= True):
 def line2d_x(x0, y0, x1, y1):
     for pt in ogl_line2d_2((x0, y0), (x1, y1)):
         yield pt
+
+from math import fabs,fmod
+
+def line2d_norm(x0, y0, x1, y1):
+    dx = x1 - x0
+    dy = y1 - y0
+    adx = fabs(dx)
+    ady = fabs(dy)
+    sx = int(x0)
+    sy = int(y0)
+    ex = int(x1)
+
+    pt0_in_diamond = (fabs(fmod(x0,1.0)-0.5) + fabs(fmod(y0,1.0)-0.5)) < 0.5
+    pt1_in_diamond = (fabs(fmod(x1,1.0)-0.5) + fabs(fmod(y1,1.0)-0.5)) < 0.5
+
+    if dx >= 0.0:
+        pt0_interior = fmod(x0,1.0) > 0.5
+        pt1_exterior = fmod(x1,1.0) > 0.5
+        xinc = 1
+        d_1 = ady * (sx+1.5-x0)
+    else:
+        pt0_interior= fmod(x0,1.0) < 0.5
+        pt1_exterior= fmod(x1,1.0) < 0.5
+        xinc = -1
+        d_1 = -ady * (sx-0.5-x0)
+
+    if dy >= 0.0:
+        yinc = 1
+        d_2 = -adx*(sy+1.0-y0)
+    else:
+        yinc = -1
+        d_2 = adx*(sy-y0)
+
+    D = d_1 + d_2
+    dNE = (ady - adx)
+    dN = (ady)
+
+    skipPxl = 0
+    if pt0_interior:
+        if not pt0_in_diamond:
+            skipPxl = 1
+    else:
+        if (D-dN) >= 0.0:
+            sy += yinc
+            D += dNE-dN
+
+    y = sy
+    x = sx
+    while x != ex:
+        if skipPxl:
+            skipPxl = 0
+        else:
+            yield (x, y)
+        if D >= 0.0:
+            y += yinc
+            D += dNE
+        else:
+            D += dN
+        x += xinc
+
+    if pt1_exterior and not pt1_in_diamond:
+        yield (ex, y)
